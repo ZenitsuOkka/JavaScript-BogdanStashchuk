c8718afa4ffa757676dfb85a821277753ab936b3
ZenitsuOkka <zenitsu.okka@gmail.com>
2022-04-13 14:49:46 +0300

    rename import as

--- modules/moduleTwo.mjs
+++ modules/moduleTwo.mjs
@@ -2,10 +2,9 @@
 
 // printMyName() //Andrej
 
-import {sum, mult} from './moduleOne.mjs'
+import {sum, mult as multNumber} from './moduleOne.mjs'
 
 const  res1 = sum(10, 2)
 console.log(res1)
 
-const  res2 = mult(10, 2)
-console.log(res2)
+console.log(multNumber(10, 2))
